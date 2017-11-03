50cef7eba3b7cb8aa48ab6d549a5578fe8afa99a
Trond Erling Hundal <trond.erling@gmail.com>
2017-11-03 22:23:05 +0100

    asd

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -1,3 +1,108 @@
+const walls = [];
+
+function addWall(wall){
+  if (walls.find((item) => {
+      return wall.x === item.x && wall.y === item.y;
+    })) {
+   walls.push(wall);
+  }
+}
+
+function areWeInEnemyRange(body) {
+  const me = body.you;
+  const enemy = body.enemies.pop();
+  let wallsArray = [];
+  if (enemy.x === me.x) {
+    if (enemy.y < me.y && enemy.direction === 'bottom') {
+      wallsArray = walls.filter(wall => {
+        return wall.x === me.x && wall.y < me.y && wall.y > enemy.y;
+      });
+    } else if (me.direction === 'top' && enemy.y > me.y) {
+      wallsArray = walls.filter(wall => {
+        return wall.x === me.x && wall.y < enemy.y && wall.y > me.y;
+      });
+    }
+    if (wallsArray.length){
+      return false;
+    } else {
+      if (me.y - enemy.y < enemy.weaponRange) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  } else if (enemy.y === me.y) {
+    if (me.direction === 'right' && me.x > enemy.x) {
+      wallsArray = walls.filter(wall => {
+        return wall.y === me.y && wall.x > enemy.x && wall.x < me.x;
+      });
+    } else if(me.direction === 'left' && enemy.x > me.x) {
+      wallsArray = walls.filter(wall => {
+        return wall.y === me.y && wall.x < enemy.x && wall.x > me.x;
+      });
+    }
+
+    if (wallsArray.length){
+      return false;
+    } else {
+      if (me.x - enemy.x < enemy.weaponRange) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
+
+function inShootingRange(body){
+  const me = body.you;
+  const enemy = body.enemies.pop();
+  let wallsArray = [];
+  if (enemy.x === me.x) {
+    if (enemy.y > me.y && me.direction === 'bottom') {
+      wallsArray = walls.filter(wall => {
+        return wall.x === me.x && wall.y > me.y && wall.y < enemy.y;
+      });
+    } else if (me.direction === 'top' && enemy.y < me.y) {
+      wallsArray = walls.filter(wall => {
+        return wall.x === me.x && wall.y < me.y && wall.y > enemy.y;
+      });
+    }
+    if (wallsArray.length){
+      return false;
+    } else {
+      if (enemy.y - me.y < me.weaponRange) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  } else if (enemy.y === me.y) {
+    if (me.direction === 'right' && enemy.x > me.x) {
+      wallsArray = walls.filter(wall => {
+        return wall.y === me.y && wall.x > me.x && wall.x < enemy.x;
+      });
+    } else if(me.direction === 'left' && enemy.x < me.x) {
+      wallsArray = walls.filter(wall => {
+        return wall.y === me.y && wall.x < me.x && wall.x > enemy.x;
+      });
+    }
+
+    if (wallsArray.length){
+      return false;
+    } else {
+      if (enemy.x - me.x < me.weaponRange) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+  }
+
+
+};
+
 function doSomethingRandom(){
   var commands = [
     "rotate-right",
@@ -13,8 +118,8 @@ function doSomethingRandom(){
 
 function info(){
   return {
-    name: "Emperor Atle",
-    team: "Sudo"
+    name: "Mr. Randombird",
+    team: "The best team"
   }
 }
 
@@ -24,18 +129,28 @@ function action (body){
   };
 }
 
+
+function getCommand(body){
+
+  const me = body.you;
+  const enemies = body.enemies;
+  const walls = body.walls;
+
+  walls.map(addWall);
+
+}
+
 function getBody(req){
   if (req.query.path === "/command") {
 
     return  {
-      command: doSomethingRandom()
+      command: getCommand(JSON.parse(req.body))
     };
 
   }
   if (req.query.path === "/info") {
     return info();
   }
-
 }
 
 module.exports = function (context, req) {
@@ -46,3 +161,4 @@ module.exports = function (context, req) {
   };
   context.done();
 };
+
