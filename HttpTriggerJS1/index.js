a3eb2529ad0c2c5bd8c9fd5f19460ae69e7f822e
Nikhil Andre Luthra <nikhil.a.luthra@gmail.com>
2017-11-03 19:21:22 +0100

    tester

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -1,43 +1,48 @@
 function doSomethingRandom(){
-    var commands = [
-        "rotate-right",
-        "rotate-left",
-        "advance",
-        "retreat",
-        "shoot"
-    ];
-    var rnd = Math.floor(Math.random() * 5);
+  var commands = [
+    "rotate-right",
+    "rotate-left",
+    "advance",
+    "retreat",
+    "shoot"
+  ];
+  var rnd = Math.floor(Math.random() * 5);
 
-    return commands[rnd];
+  return commands[rnd];
 }
 
 function info(){
-    return {
-        name: "Mr. Randombird",
-        team: "The best team"
-    }
+  return {
+    name: "Mr. Randombird",
+    team: "The best team"
+  }
 }
 
 function action (body){
-    return {
-            command: doSomethingRandom()
-            };
+  return {
+    command: doSomethingRandom()
+  };
 }
 
 function getBody(req){
-    switch(req.method){
-        case 'GET':
-            return info();
-        case 'POST':
-            return action(req);
-    }
+  if (req.query.path === "/command") {
+
+    return  {
+      command: 'shoot'
+    };
+
+  }
+  if (req.query.path === "/info") {
+    return info();
+  }
+
 }
 
 module.exports = function (context, req) {
 
-    context.log('JavaScript HTTP trigger function processed a request.', req.body);
-    context.res = {
-        body: getBody(req)
-    };
-    context.done();
+  context.log('JavaScript HTTP trigger function processed a request.', req.body);
+  context.res = {
+    body: getBody(req)
+  };
+  context.done();
 };
