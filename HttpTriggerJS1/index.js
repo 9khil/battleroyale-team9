4c5c66b0ad9c886e7b37dbd14b76f9c5deea03d1
Adam Nuridov <adamnuri92@gmail.com>
2017-11-04 09:07:28 +0100

    check retreat

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -243,6 +243,33 @@ function enemyRight(){
   return me.x < enemy.x;
 }
 
+function getRetreatMove(){
+  let nextX, nextY;
+  if (me.direction === 'right'){
+    nextX = me.x - 1;
+    nextY = me.y;
+  } else if (me.direction === 'left' ) {
+    nextX = me.x + 1 ;
+    nextY = me.y;
+  } else if (me.direction ==='top'){
+    nextX = me.x;
+    nextY = me.y + 1;
+  } else {
+    nextX = me.x ;
+    nextY = me.y - 1;
+  }
+
+  if (nextX > 0 && nextX < body.mapWidth && nextY > 0 &&
+    nextY < body.mapHeight && !walls.find(wall => (wall.x === nextX && wall.y === nextY))) {
+    return 'retreat';
+  } else {
+    /* Todo: Choose a wiser path here
+        - Need to choose a smarter direction, not into walls
+     */
+    return 'rotate-right';
+  }
+}
+
 function getMove(){
   let nextX, nextY;
   if (me.direction === 'right'){
@@ -305,7 +332,7 @@ function getCommand(request) {
     } else if (enemyInOurShootingSight(body) && areWeInEnemyShootingSight(body) && shouldShoot()) {
       return 'shoot';
     } else if(areWeInEnemyShootingSight(body)){ //TODO dont retreat into a wall
-      return 'retreat';
+      return getRetreatMove();
     }
     const move = enemyAdvancementMove();
     if (move) return move;
